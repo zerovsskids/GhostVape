540d0f9856c916f1036bd37b1a0cd8341413d0ae
Xai <mirowitchikase122@gmail.com>
2025-08-17 15:28:13 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
-if shared.vape then shared.vape:Uninject() end
+if shared.GhostVape then shared.GhostVape:Uninject() end
 
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
@@ -8,11 +8,11 @@ if identifyexecutor then
 	end
 end
 
-local vape
+local GhostVape
 local loadstring = function(...)
 	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	if err and GhostVape then
+		GhostVape:CreateNotification('GhostVape', 'Failed to load : '..err, 30, 'alert')
 	end
 	return res
 end
@@ -31,13 +31,13 @@ local playersService = cloneref(game:GetService('Players'))
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/zerovsskids/GhostVape/'..readfile('GhostVape/profiles/commit.txt')..'/'..select(1, path:gsub('GhostVape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after GhostVape updates.\n'..res
 		end
 		writefile(path, res)
 	end
@@ -45,73 +45,73 @@ local function downloadFile(path, func)
 end
 
 local function finishLoading()
-	vape.Init = nil
-	vape:Load()
+	GhostVape.Init = nil
+	GhostVape:Load()
 	task.spawn(function()
 		repeat
-			vape:Save()
+			GhostVape:Save()
 			task.wait(10)
-		until not vape.Loaded
+		until not GhostVape.Loaded
 	end)
 
 	local teleportedServers
-	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
+	GhostVape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
+		if (not teleportedServers) and (not shared.GhostVapeIndependent) then
 			teleportedServers = true
 			local teleportScript = [[
-				shared.vapereload = true
-				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
+				shared.GhostVapereload = true
+				if shared.GhostVapeDeveloper then
+					loadstring(readfile('GhostVape/loader.lua'), 'loader')()
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/zerovsskids/GhostVape/'..readfile('GhostVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
 			]]
-			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+			if shared.GhostVapeDeveloper then
+				teleportScript = 'shared.GhostVapeDeveloper = true\n'..teleportScript
 			end
-			if shared.VapeCustomProfile then
-				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			if shared.GhostVapeCustomProfile then
+				teleportScript = 'shared.GhostVapeCustomProfile = "'..shared.GhostVapeCustomProfile..'"\n'..teleportScript
 			end
-			vape:Save()
+			GhostVape:Save()
 			queue_on_teleport(teleportScript)
 		end
 	end))
 
-	if not shared.vapereload then
-		if not vape.Categories then return end
-		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+	if not shared.GhostVapereload then
+		if not GhostVape.Categories then return end
+		if GhostVape.Categories.Main.Options['GUI bind indicator'].Enabled then
+			GhostVape:CreateNotification('Finished Loading', GhostVape.GhostVapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(GhostVape.Keybind, ' + '):upper()..' to open GUI', 5)
 		end
 	end
 end
 
-if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'new')
+if not isfile('GhostVape/profiles/gui.txt') then
+	writefile('GhostVape/profiles/gui.txt', 'new')
 end
-local gui = readfile('newvape/profiles/gui.txt')
+local gui = readfile('GhostVape/profiles/gui.txt')
 
-if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
+if not isfolder('GhostVape/assets/'..gui) then
+	makefolder('GhostVape/assets/'..gui)
 end
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
-shared.vape = vape
+GhostVape = loadstring(downloadFile('GhostVape/guis/'..gui..'.lua'), 'gui')()
+shared.GhostVape = GhostVape
 
-if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-	if isfile('newvape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+if not shared.GhostVapeIndependent then
+	loadstring(downloadFile('GhostVape/games/universal.lua'), 'universal')()
+	if isfile('GhostVape/games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('GhostVape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 	else
-		if not shared.VapeDeveloper then
+		if not shared.GhostVapeDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet(''..readfile('GhostVape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+				loadstring(downloadFile('GhostVape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 			end
 		end
 	end
 	finishLoading()
 else
-	vape.Init = finishLoading
-	return vape
+	GhostVape.Init = finishLoading
+	return GhostVape
 end
